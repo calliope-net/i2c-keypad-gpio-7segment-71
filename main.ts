d3354d514d9e2f8b8e56abbe1669f5f31493876c
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-11-14 20:25:19 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,46 @@
+function toByte (A: number, B: number, C: number, D: number, E: number, F: number, G: number) {
+    byte = 0
+}
 pins.onKeyboardEvent(function (zeichenCode, zeichenText, isASCII) {
     if (pins.between(zeichenCode, 48, 57)) {
-    	
+        select(zeichenCode - 48)
     }
 })
 function select (Zahl: number) {
     if (Zahl == 0) {
-        Ziffer(1, 1, 1, 1, 1, 1, 0)
+        toByte(1, 1, 1, 1, 1, 1, 0)
     } else if (Zahl == 1) {
-        Ziffer(0, 1, 1, 0, 0, 0, 0)
+        toByte(0, 1, 1, 0, 0, 0, 0)
     } else if (Zahl == 2) {
-        Ziffer(1, 1, 1, 1, 0, 0, 1)
+        toByte(1, 1, 0, 1, 1, 0, 1)
     } else if (Zahl == 3) {
-        Ziffer(1, 1, 1, 0, 0, 0, 0)
+        toByte(1, 1, 1, 1, 0, 0, 1)
     } else if (Zahl == 4) {
-        Ziffer(1, 1, 1, 1, 1, 1, 1)
+        toByte(0, 1, 1, 0, 0, 1, 1)
     } else if (Zahl == 5) {
-        Ziffer(0, 1, 1, 0, 0, 0, 0)
+        toByte(1, 0, 1, 1, 0, 1, 1)
     } else if (Zahl == 6) {
-        Ziffer(0, 1, 1, 0, 0, 0, 0)
+        toByte(1, 0, 1, 1, 1, 1, 1)
     } else if (Zahl == 7) {
-        Ziffer(1, 1, 1, 0, 0, 0, 0)
+        toByte(1, 1, 1, 0, 0, 0, 0)
     } else if (Zahl == 8) {
-        Ziffer(0, 1, 1, 0, 0, 0, 0)
+        toByte(1, 1, 1, 1, 1, 1, 1)
     } else if (Zahl == 9) {
-        Ziffer(1, 1, 1, 1, 0, 1, 1)
+        toByte(1, 1, 1, 1, 0, 1, 1)
     } else if (Zahl == 10) {
-        Ziffer(1, 1, 1, 0, 1, 1, 1)
+        toByte(1, 1, 1, 0, 1, 1, 1)
     } else if (Zahl == 11) {
-        Ziffer(0, 0, 1, 1, 1, 1, 1)
+        toByte(0, 0, 1, 1, 1, 1, 1)
     } else if (Zahl == 12) {
-        Ziffer(0, 0, 0, 0, 1, 1, 1)
+        toByte(1, 0, 0, 1, 1, 1, 0)
     } else if (Zahl == 13) {
-        Ziffer(0, 1, 1, 0, 1, 1, 0)
+        toByte(0, 1, 1, 1, 1, 0, 1)
     } else if (Zahl == 14) {
-        Ziffer(1, 1, 1, 1, 0, 0, 1)
+        toByte(1, 0, 0, 1, 1, 1, 1)
     } else if (Zahl == 15) {
-        Ziffer(1, 0, 0, 0, 1, 1, 1)
+        toByte(1, 0, 0, 0, 1, 1, 1)
     } else {
-        Ziffer(0, 0, 0, 0, 0, 0, 1)
+        toByte(0, 0, 0, 0, 0, 0, 1)
     }
 }
 function Ziffer (A: number, B: number, C: number, D: number, E: number, F: number, G: number) {
@@ -50,16 +53,17 @@ function Ziffer (A: number, B: number, C: number, D: number, E: number, F: numbe
     pins.digitalWritePin(DigitalPin.C10, 1 - G)
     pins.digitalWritePin(DigitalPin.C11, 0)
 }
+let byte = 0
 qwiicgpio.beimStart(qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27))
 qwiicgpio.setMode(
 qwiicgpio.qwiicgpio_eADDR(qwiicgpio.eADDR.GPIO_x27),
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
-qwiicgpio.eIO.IN,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
+qwiicgpio.eIO.OUT,
 qwiicgpio.eIO.OUT
 )
 loops.everyInterval(500, function () {
